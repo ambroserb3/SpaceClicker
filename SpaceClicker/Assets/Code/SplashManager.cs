f3beb8316c9f4d30b8c8376eee307de972bae266
stephanrb3 <stephanrb3@gmail.com>
2019-02-26 18:24:46 -0500

    Added Splash and Title Scene
    Basic Splash with CU text filler and manager template
    Title Screen scene with Play & How To buttons. Also Time Limit Dropdown
    All scenes in build index

---
+++ SpaceClicker/Assets/Code/SplashManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour
+{
+    public GameObject SplashManage;
+    [SerializeField]
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(SplashManage);
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.buildIndex == 0 && timer >= 3)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
