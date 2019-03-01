d72062a26a25800f97f862f6df48ab763f8d09fb
Mustafa Eyceoz <me2680@columbia.edu>
2019-03-01 00:11:04 -0500

    Working Counter uptick
    One planet, one resource

---
+++ SpaceClicker/Assets/Planets with Space Background in Flat Style/PlanetManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public int water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        water = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        print("huzzah");
+        water++;
+        GameObject.Find("water").GetComponent<TextMesh>().text = "Water: " + water;
+    }
+}
