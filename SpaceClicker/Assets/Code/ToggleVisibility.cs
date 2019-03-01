79d86e56fd1cbd4d555f2883f7379e8809261037
RamiMatar <rami.matar.00@gmail.com>
2019-02-28 23:47:01 -0500

    ui elements
    added button to scrollable menu

---
+++ SpaceClicker/Assets/Code/ToggleVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleVisibility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool visibility;
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        visibility = false;
+    }
+    
+    public void ToggleState()
+    {
+        transform.GetChild(0).gameObject.SetActive(!visibility);
+        visibility = !visibility;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
