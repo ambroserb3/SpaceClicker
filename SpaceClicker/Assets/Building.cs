97a1d10629c5808d014e6c5a189d7b66a0371c95
RamiMatar <rami.matar.00@gmail.com>
2019-03-09 13:32:05 -0500

    building manager

---
+++ SpaceClicker/Assets/Building.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Code.Buildings{
+    public class Building 
+    {
+        protected int[] resourceGeneration;
+
+        public Building(int[] resourceGeneration){
+            this.resourceGeneration = resourceGeneration;
+        }
+        public override string ToString(){
+            string st = "";
+            for(int i = 0; i < resourceGeneration.Length; i++){
+                st += (i + ": " + resourceGeneration[i]);
+            }
+            return "Building with resource generation of:" + st;
+        }
+        
+    }
+}
