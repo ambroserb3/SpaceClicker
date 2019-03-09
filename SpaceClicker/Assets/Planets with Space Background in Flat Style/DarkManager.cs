48cad59c28ca46b3af511c2194e7f015983f45c0
Mustafa Eyceoz <me2680@columbia.edu>
2019-03-09 12:54:11 -0500

    Skeletor
    Backbone formed, game manager + planet scripts

---
+++ SpaceClicker/Assets/Planets with Space Background in Flat Style/DarkManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkManager : MonoBehaviour
+{
+    private int[] resources;
+    private int[] buildings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildings = new int[6];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resources = PlanetManager.getResources();
+    }
+
+
+    private void OnMouseDown()
+    {
+        print("huzzah");
+        resources[0] = resources[0] + 5;
+        resources[1] = resources[1] + 5;
+        resources[2] = resources[2] + 3;
+        resources[3] = resources[3] + 1;
+        resources[4] = resources[4] + 2;
+        resources[5] = resources[5] + 4;
+
+        PlanetManager.resources = resources;
+
+    }
+}
--- SpaceClicker/Assets/Planets with Space Background in Flat Style/PlanetManager.cs
+++ SpaceClicker/Assets/Planets with Space Background in Flat Style/PlanetManager.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
-    public int water;
+    public static int[] resources;
 
     // Start is called before the first frame update
     void Start()
     {
-        water = 0;
+        resources = new int[6];
+        resources[0] = 0;
+        resources[1] = 0;
+        resources[2] = 0;
+        resources[3] = 0;
+        resources[4] = 0;
+        resources[5] = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject.Find("water").GetComponent<TextMesh>().text = "Water: " + resources[0];
+        GameObject.Find("power").GetComponent<TextMesh>().text = "Power: " + resources[1];
+        GameObject.Find("food").GetComponent<TextMesh>().text = "Food: " + resources[2];
+        GameObject.Find("khyber").GetComponent<TextMesh>().text = "Khyber: " + resources[3];
+        GameObject.Find("titanium").GetComponent<TextMesh>().text = "Titanium: " + resources[4];
+        GameObject.Find("iridium").GetComponent<TextMesh>().text = "Iridium: " + resources[5];
     }
 
-    private void OnMouseDown()
+    public static int[] getResources()
     {
-        print("huzzah");
-        water++;
-        GameObject.Find("water").GetComponent<TextMesh>().text = "Water: " + water;
+        return resources;
     }
+
 }
