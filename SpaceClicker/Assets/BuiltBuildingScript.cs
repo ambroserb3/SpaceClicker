91c25a6b46746735f592f3d1f8274a7cc22a659d
JamesBollas <jbb2170@columbia.edu>
2019-03-09 15:19:11 -0500

    building scripts

--- SpaceClicker/Assets/BuiltBuildingScript.cs
+++ SpaceClicker/Assets/BuiltBuildingScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BuiltBuildingScript : MonoBehaviour
 {
     public string buildingtype;
+    public string planetcolliding;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +19,8 @@ public class BuiltBuildingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(buildingtype);
-        gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) + new Vector3(0, 0, 5);
+        //print(buildingtype);
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) + new Vector3(0, 0, 10);
         if (Input.GetMouseButtonUp(0))
         {
             Destroy(gameObject);
@@ -27,7 +28,18 @@ public class BuiltBuildingScript : MonoBehaviour
     }
     void OnMouseUp()
     {
-        //print("hi");
+        print(planetcolliding);
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("hiay");
+        planetcolliding = other.gameObject.name;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        planetcolliding = "";
     }
 }
