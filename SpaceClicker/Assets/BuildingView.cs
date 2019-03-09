e57af66adbfdaee6cc0719f96922fc023e31fa0d
RamiMatar <rami.matar.00@gmail.com>
2019-03-09 12:16:37 -0500

    Added building views to buildings menu

---
+++ SpaceClicker/Assets/BuildingView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject A;
+    private string[] names = new string[5] {"A","B","C","D","E"};
+    private GameObject[] gameObjects = new GameObject[5];
+    void Start()
+    {
+        float height = ((RectTransform)A.transform).rect.height + 15;
+        Vector3 initialPosition = GameObject.Find("Building").transform.localPosition;
+        for(int i = 0; i < names.Length; i++){
+            gameObjects[i] = Instantiate(A, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+            gameObjects[i].transform.parent = GameObject.Find("Content").transform;
+            gameObjects[i].transform.localScale = new Vector3(1,1,1);
+            gameObjects[i].transform.localPosition = initialPosition - new Vector3(0, height * (i+1), 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
