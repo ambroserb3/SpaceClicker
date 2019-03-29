f987a9e02757d92e739240057e136344d91f141a
stephanrb3 <stephanrb3@gmail.com>
2019-03-29 18:20:56 -0400

    main fixed?

---
+++ SpaceClicker/Assets/Code/HowTo1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowTo1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HowTo()
+    {
+        SceneManager.LoadScene("HowTo2");
+    }
+    public void HowTo2()
+    {
+        SceneManager.LoadScene("HowTo2");
+    }
+    public void HowTo3()
+    {
+        SceneManager.LoadScene("HowTo3");
+    }
+    public void goHome()
+    {
+        SceneManager.LoadScene("title");
+    }
+}
