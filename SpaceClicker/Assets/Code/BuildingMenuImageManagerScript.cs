e3e18a4ddff9ea75758dc4c7d09c465dcab26640
JamesBollas <jbb2170@columbia.edu>
2019-03-29 19:59:25 -0400

    Building Image Behavior Added

---
+++ SpaceClicker/Assets/Code/BuildingMenuImageManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMenuImageManagerScript : MonoBehaviour
+{
+    public Sprite PowerImage;
+    public Sprite MineImage;
+    public Sprite BImage;
+    public Sprite CImage;
+    public Sprite EImage;
+    public Sprite FImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public Sprite getSprite(string name){
+        if (name == "Power"){
+            return PowerImage;
+        }
+        if (name == "Mine"){
+            return MineImage;
+        }
+        return BImage;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
