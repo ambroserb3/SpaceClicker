0598b4688721dc57a9645f44431ce78d420dde83
RamiMatar <rami.matar.00@gmail.com>
2019-03-29 20:30:05 -0400

    Variable name change

--- SpaceClicker/Assets/Code/BuildingView.cs
+++ SpaceClicker/Assets/Code/BuildingView.cs
@@ -6,16 +6,16 @@ public class BuildingView : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject A;
-    private string[] names = new string[5] {"Mine","Farm","Habitation","WTF","Synth"};
+    public static string[] BUILDING_NAMES = new string[5] {"Mine","Farm","Habitation","WTF","Synth"};
     private GameObject[] gameObjects = new GameObject[5];
     void Start()
     {
         float height = ((RectTransform)A.transform).rect.height + 15;
         Vector3 initialPosition = GameObject.Find("Building").transform.localPosition;
-        for(int i = 0; i < names.Length; i++){
+        for(int i = 0; i < BUILDING_NAMES.Length; i++){
             gameObjects[i] = Instantiate(A, new Vector3(0,0,0), Quaternion.identity) as GameObject;
             gameObjects[i].transform.parent = GameObject.Find("Content").transform;
-            gameObjects[i].GetComponent<BuildingBehavior>().SetName(names[i]);
+            gameObjects[i].GetComponent<BuildingBehavior>().SetName(BUILDING_NAMES[i]);
             gameObjects[i].transform.localScale = new Vector3(1,1,1);
             gameObjects[i].transform.localPosition = initialPosition - new Vector3(0, height * (i+1), 0);
         }
