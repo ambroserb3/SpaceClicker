9c0667e3974b458070abdfa1bf2bbd9b2abd9212
RamiMatar <rami.matar.00@gmail.com>
2019-03-29 20:30:41 -0400

    Added Resource Optimization Methods

--- SpaceClicker/Assets/Code/Resources.cs
+++ SpaceClicker/Assets/Code/Resources.cs
@@ -13,6 +13,8 @@ namespace Code{
         // 5 : KHYBERIUM
 
         public static int NUM_OF_RESOURCES = 6;
+        
+    
         private int[] resources;
 
         public Resources(){
@@ -26,6 +28,52 @@ namespace Code{
                 resources[i] = resourcesArray[i];
             }
         }
+        public static Resources EarthResourceGeneration(){
+            return new Resources(new int[] {10,10,10,5,5,0});
+        }
+        public static Resources WaterResourceGeneration(){
+            return new Resources(new int[] {10,10,10,5,5,0});
+        }
+        public static Resources SandResourceGeneration(){
+            return new Resources(new int[] {10,10,10,5,5,0});
+        }
+        public static Resources DarkResourceGeneration(){
+            return new Resources(new int[] {10,10,10,5,5,0});
+        }
+        //INPUT: NAME AND INT ARRAY
+        //OUTPUT: 
+        public static Resources BuildingResourceOptimizer(string planetName, int[] buildings){
+            if(buildings.Length != BuildingView.BUILDING_NAMES.Length)
+                return null;
+            if(planetName == "Earth-Planet")
+                return new Resources(new int[]{buildings[0] * 5, buildings[1] * 4, buildings[2] * 4, buildings[3] * 3, buildings[4] * 3, buildings[5] * 4});
+            if(planetName == "Water-Planet")
+                return  new Resources(new int[]{buildings[0] * 3, buildings[1] * 3, buildings[2] * 2, buildings[3] * 4, buildings[4] * 2, buildings[5] * 3});
+            if(planetName == "Sand-Planet")
+                return  new Resources(new int[]{buildings[0] * 2, buildings[1] * 4, buildings[2] * 3, buildings[3] * 2, buildings[4] * 1, buildings[5] * 4});
+            if(planetName == "Dark-Planet")
+                return  new Resources(new int[]{buildings[0] * 0, buildings[1] * 2, buildings[2] * 1, buildings[3] * 1, buildings[4] * 4, buildings[5] * 2});
+            return null;
+
+        }
+        public static Resources GetPlanetResourceGeneration(string planetName){
+            if(planetName == "Earth-Planet")
+                return EarthResourceGeneration();
+            if(planetName == "Water-Planet")
+                return WaterResourceGeneration();
+            if(planetName == "Sand-Planet")
+                return SandResourceGeneration();
+            if(planetName == "Dark-Planet")
+                return DarkResourceGeneration();
+            return null;
+        }
+        public static Resources GetOptimizedPlanetGeneration(string planetName, int[] buildings){
+            Resources planetResources = GetPlanetResourceGeneration(planetName);
+            Resources optimizedBuildingResources = BuildingResourceOptimizer(planetName, buildings);
+            return planetResources.addResources(optimizedBuildingResources);
+        }
+
+
         public int[] GetResources(){
             return resources;
         }
