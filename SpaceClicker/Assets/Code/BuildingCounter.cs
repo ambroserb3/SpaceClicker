723c76a8949ca40d35085590bed5e18bfc05c9bd
stephanrb3 <stephanrb3@gmail.com>
2019-04-05 18:01:58 -0400

    BuildingCountText

--- SpaceClicker/Assets/Code/BuildingCounter.cs
+++ SpaceClicker/Assets/Code/BuildingCounter.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingCounter : MonoBehaviour{
+
+    public Text buildingCount;
     public int buildings;
     void Start(){
+        buildingCount.text = "";
         buildings = 0;
     }
     void Update(){
-
+        buildingCount.text = "" + buildings;
     }
     public void addBuilding(){
         buildings++;
